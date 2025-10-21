8e4124b418380f0e3001c5e159756a3ec58f8560
Maria-Narvaez <marianarvaez0808@gmail.com>
2025-10-20 22:11:40 -0500

    CAMBIO

---
+++ ejercicio1.py
@@ -0,0 +1,33 @@
+#captura por teclado
+
+nombre = input("regalame tu nombre: ")
+print("Tu nombre es: " , nombre)
+
+#variables, tipos de datos y operador
+
+nombre ="Maria"
+telefono ="12345668910"
+direccion ="miami"
+correo ="marianarvaez0808@gmail.com"
+edad = "22"
+
+print("mi nombre es: " + nombre + " y mi telefono es " + telefono + ", tengo una direccion que es " + direccion)
+print(f"Mi nombre es: {nombre} y mi telefono es {telefono}, tengo una direccion que es {direccion}")
+
+#DATOS ENTEROS
+numero = 0
+numero_decimal = 5.0
+saludo = "Hola a todos"
+Datos_tipo_logico = True #ó False
+
+#operadores aritmeticos
+a = 8
+b = 10
+
+c,d = 8,10
+
+print(f"Suma = {a+b}")
+print(f"Suma = {c+d}")
+
+#DESARROLLAR LA -, *,/,//,**
+
